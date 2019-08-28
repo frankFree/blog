3d4d7c81217b6f147f5d9e54ad48f0db42b01816
frankFree <curryliangdong@gmail.com>
2019-08-28 14:59:23 +0800

    [dev]update

--- src/request/index.js
+++ src/request/index.js
@@ -15,7 +15,7 @@ export const getUserInfo = (params = {}) => {
   params.client_id = Config.user.client_id
   params.client_secret = Config.user.client_secret
   return axios.request({
-    url: '/users/loo41',
+    url: '/users/frankfree',
     params,
     method: 'get'
   })
